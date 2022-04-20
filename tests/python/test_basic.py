7629d47473f38a7bb1cb2c99a43b79a39aeaf39a
wudangt <786750893@qq.com>
2022-04-20 21:00:08 +0800

    add expr simplify and canonical

--- tests/python/test_basic.py
+++ tests/python/test_basic.py
@@ -16,6 +16,20 @@ def test_basic():
     assert ovm.format_str(c) == '(%s + %s)' % (a.name, b.name)
 
 
+def test_simplify():
+    a = ovm.Var('a')
+    b = ovm.Var('b')
+    e1 = a * (2 + 1) + b * 1
+    e2 = a * (2 + 1) - b * 1
+    e3 = ovm.max(a * 3.3 + 5, 3 + 3.3 * a)
+    e4 = a - a
+    assert ovm.format_str(ovm.simplify(e1)) == '((%s * 3) + %s)' % (a.name, b.name)
+    assert ovm.format_str(ovm.simplify(e2)) == '((%s * 3) + (%s * -1))' % (a.name, b.name)
+    assert ovm.format_str(ovm.simplify(e3)) == '((%s * 3.3) + 5)' % (a.name)
+    assert ovm.format_str(ovm.simplify(e4)) == '0'
+
 if __name__ == "__main__":
+    test_simolify()
     test_basic()
     test_bind()
+    
