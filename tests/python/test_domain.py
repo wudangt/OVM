7b26df7c3bdc45f51eade850bccf7152189d9007
wudangt <786750893@qq.com>
2022-04-21 21:26:14 +0800

    Fold RTensor into tensor

--- tests/python/test_domain.py
+++ tests/python/test_domain.py
@@ -1,24 +1,26 @@
-import ovm
+import tvm
 
 def test_range_infer():
-    x = ovm.Var('x')
-    y = ovm.Var('y')
-    t = ovm.Var('t')
+    x = tvm.Var('x')
+    y = tvm.Var('y')
+    t = tvm.Var('t')
     z = x + y + t
-    zr = ovm.infer_range(z, {x: ovm.Range(10, 20), y : ovm.Range(10, 11)})
+    zr = tvm.infer_range(z, {x: tvm.Range(10, 20), y : tvm.Range(10, 11)})
     assert str(zr) == "((t0 + 20), (t0 + 30))"
 
 def test_tensor_dom_infer():
-    A = ovm.Tensor(2, name='A')
-    B = ovm.Tensor(2, name='B')
-    T = ovm.Tensor(3, lambda i, j, k: A(i, k) * B(j, k),
-                   shape=(A.shape[0], B.shape[0], A.shape[1]))
-    rd = ovm.RDom(ovm.Range(A.shape[1]))
-    C = ovm.Tensor(2, lambda i, j: ovm.reduce_sum(T(i, j, rd.index[0]), rdom=rd),
+    A = tvm.Tensor(2, name='A')
+    B = tvm.Tensor(2, name='B')
+    rd = tvm.RDom(tvm.Range(A.shape[1]))
+    T = tvm.Tensor(2, lambda i, j:
+                   tvm.reduce_sum(A(i, rd.index[0]) * B(j, rd.index[0]), rdom=rd),
+                   shape=(A.shape[0], B.shape[0]))
+    C = tvm.Tensor(2, lambda i, j: T(i,j),
                    shape=(A.shape[0], B.shape[0]))
 
-    cdom = [ovm.Range(0, 10), ovm.Range(1, 11)]
+    cdom = [tvm.Range(0, 10), tvm.Range(1, 11)]
     tdom = C.infer_input_domains(cdom, inputs=[T])[T]
+    assert T.is_rtensor
     assert str(tdom[0]) == "(0, 10)"
 
 
