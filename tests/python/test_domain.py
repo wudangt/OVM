87380746a9d46f0bc87c6da3be0576ff04c82113
wudangt <786750893@qq.com>
2022-04-21 21:19:02 +0800

    fixed the test_domain

--- tests/python/test_domain.py
+++ tests/python/test_domain.py
@@ -1,91 +1,27 @@
-from __future__ import absolute_import as _abs
-from . import expr as _expr
-from . import expr_util as _expr_util
-from . import domain as _dom
+import ovm
+
+def test_range_infer():
+    x = ovm.Var('x')
+    y = ovm.Var('y')
+    t = ovm.Var('t')
+    z = x + y + t
+    zr = ovm.infer_range(z, {x: ovm.Range(10, 20), y : ovm.Range(10, 11)})
+    assert str(zr) == "((t0 + 20), (t0 + 30))"
+
+def test_tensor_dom_infer():
+    A = ovm.Tensor(2, name='A')
+    B = ovm.Tensor(2, name='B')
+    T = ovm.Tensor(3, lambda i, j, k: A(i, k) * B(j, k),
+                   shape=(A.shape[0], B.shape[0], A.shape[1]))
+    rd = ovm.RDom(ovm.Range(A.shape[1]))
+    C = ovm.Tensor(2, lambda i, j: ovm.reduce_sum(T(i, j, rd.index[0]), rdom=rd),
+                   shape=(A.shape[0], B.shape[0]))
+
+    cdom = [ovm.Range(0, 10), ovm.Range(1, 11)]
+    tdom = C.infer_input_domains(cdom, inputs=[T])[T]
+    assert str(tdom[0]) == "(0, 10)"
 
 
-class Tensor(object):
-    def __init__(self, ndim, fcompute=None, name=None, shape=None):
-        self.ndim = ndim
-        if fcompute:
-            arg_names = fcompute.func_code.co_varnames
-            assert(len(arg_names) == ndim)
-            self.dim_index = [_expr.Var(n) for n in arg_names]
-            self.expr = fcompute(*self.dim_index)
-            if shape is None:
-                raise ValueError("argument shape need to be given for intermediate tensor")
-            self.shape = shape
-        else:
-            self.expr = None
-            self.dim_index = None
-            shape_name = '_shape'
-            if name: shape_name = name + shape_name
-            self.shape = shape if shape else tuple(
-                _expr.Var("%s_%d_" % (shape_name, i)) for i in range(ndim))
-
-        self.name = name if name else "TensorObj"
-        self.inputs = None
-
-    def __call__(self, *indices):
-        if len(indices) != self.ndim:
-            raise ValueError("Need to provide %d index in tensor slice" % self.ndim)
-        return _expr.TensorReadExpr(self, indices)
-
-    def input_tensors(self):
-        """List of input tensors to this tensor.
-
-        Returns
-        -------
-        inputs : list of input tensors
-        """
-        if self.inputs is not None:
-            return self.inputs
-        inputs = []
-        if self.expr:
-            def collect(e):
-                if isinstance(e, _expr.TensorReadExpr):
-                    inputs.append(e.tensor)
-            _expr_util.visit(self.expr, collect)
-        self.inputs = set(inputs)
-        return self.inputs
-
-    def infer_input_domains(self, out_domain, inputs):
-        """Infer the input domains of each domain in given inputs list.
-
-        Parameters
-        ----------
-        out_domain : list of Range
-            Domain of each dimension.
-
-        Returns
-        -------
-        in_domains: dict Tensor->Domain
-        """
-        assert self.expr
-        assert len(out_domain) == len(self.dim_index)
-        index_domains = {
-            self.dim_index[i] : out_domain[i] for i in range(len(out_domain))
-        }
-        iset = {}
-        for t in inputs:
-            assert t in self.input_tensors()
-            iset[t] = []
-
-        def prepare(e):
-            if isinstance(e, _expr.ReduceExpr):
-                rd = e.rdom
-                for i in range(len(rd.domain)):
-                    index_domains[rd.index[i]] = rd.domain[i]
-            elif isinstance(e, _expr.TensorReadExpr):
-                if e.tensor in iset:
-                    iset[e.tensor].append(e)
-        _expr_util.visit(self.expr, prepare)
-        result = {}
-        for k, v in iset.items():
-            dm = [None] * len(v[0].indices)
-            for e in v:
-                for i, idx in enumerate(e.indices):
-                    dm[i] = _dom.union_range(
-                        dm[i], _dom.infer_range(idx, index_domains, allow_unbind_var=False))
-            result[k] = dm
-        return result
+if __name__ == "__main__":
+    test_range_infer()
+    test_tensor_dom_infer()
